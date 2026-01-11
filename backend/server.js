d886e386ba16c4284f38a7121296ecb0db2f48d7
jihaduttolibinn <jihaduttolibin02@gmail.com>
2026-01-12 00:08:22 +0700

    Memperbaiki server.js di bagian app use uploads supaya bisa mengambil file untuk di apload di web fotonyaa

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,10 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/uploads', express.static('uploads'));
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/books', require('./routes/books'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.APP_PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
